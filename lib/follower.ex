07571bcf88293470c7e01741f63b43d25f0d1dee
Leonard Ge <gw980531@gmail.com>
2020-02-18 15:13:44 +0000

    Fixed state updating bug.

--- lib/follower.ex
+++ lib/follower.ex
@@ -11,7 +11,7 @@ defmodule Follower do
 
   defp next(s, timer) do
     receive do
-      { :crash_timeout } -> 
+      { :crash_timeout } ->
         Process.cancel_timer(timer)
         Monitor.debug(s, "crashed and will sleep for 1000 ms")
         Process.sleep(1000)
@@ -24,10 +24,11 @@ defmodule Follower do
        prevLogIndex, prevLogTerm,
        entries, leaderCommit, clientP} = m ->
         # TODO: not sure if needed
-        if term > s[:curr_term] do
-          s = State.voted_for(s, nil)
-          s = State.curr_term(s, term)
-        end
+        s =
+          cond do
+            term > s[:curr_term] -> State.curr_term(State.voted_for(s, nil), term)
+            true -> s
+          end
         cond do
           term < s[:curr_term] ->
             # TODO: this means leader is out-of-date right? should we send a different response to differentiate from second case
@@ -40,10 +41,10 @@ defmodule Follower do
           entries != nil ->
             s = State.log(s, Enum.concat(s[:log], (for entry <- entries, do: entry)))
             send(Enum.at(s[:servers], leaderId - 1), {:appendEntryResponse, s[:curr_term], true, m, self(), Log.getPrevLogIndex(s[:log])})
-            Monitor.debug(s, "Log updated log length #{length(s[:log])}")
+            Monitor.debug(s, "Log updated log length #{length(s[:log])} cur term: #{s[:curr_term]}")
             # update commit index if necessary
-            s = State.commit_index(s, if leaderCommit > s[:commit_index] 
-                                      do min(leaderCommit, length(s[:log])) 
+            s = State.commit_index(s, if leaderCommit > s[:commit_index]
+                                      do min(leaderCommit, length(s[:log]))
                                       else s[:commit_index] end)
             # update last applied if necessary, might have off by 1 problems
             if s[:commit_index] > s[:last_applied] do
@@ -55,11 +56,11 @@ defmodule Follower do
           # Question: how to differentiate hearbeat response from append entry response?
 
           # heartbeat
-          true -> 
+          true ->
             # Monitor.debug(s, "received hearbeat")
             send(Enum.at(s[:servers], leaderId - 1), {:appendEntryResponse, s[:curr_term], true})
-            s = State.commit_index(s, if leaderCommit > s[:commit_index] 
-                                      do min(leaderCommit, length(s[:log])) 
+            s = State.commit_index(s, if leaderCommit > s[:commit_index]
+                                      do min(leaderCommit, length(s[:log]))
                                       else s[:commit_index] end)
             # update last applied if necessary
             if s[:commit_index] > s[:last_applied] do
@@ -72,7 +73,7 @@ defmodule Follower do
 
       # TODO: voting logic not sure if entirely correct
       {:requestVote, votePid, term, candidateId, lastLogIndex, lastLogTerm} ->
-        up_to_date = lastLogTerm > Log.getPrevLogTerm(s[:log]) or 
+        up_to_date = lastLogTerm > Log.getPrevLogTerm(s[:log]) or
                     (lastLogTerm == Log.getPrevLogTerm(s[:log]) and lastLogIndex >= Log.getPrevLogIndex(s[:log]))
         cond do
           term > s[:curr_term] and up_to_date ->
@@ -99,7 +100,7 @@ defmodule Follower do
       #   next(s, timer)
 
       {:timeout} ->
-        Monitor.debug(s, "converts to candidate from follower in term #{s[:curr_term]} (+1)") 
+        Monitor.debug(s, "converts to candidate from follower in term #{s[:curr_term]} (+1)")
         Candidate.start(s)
     end
   end
